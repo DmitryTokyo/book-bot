c2b7e15779f17a26e2d5e8f13146b5c1c8c74102
andimeon-ubuntu <andimeon@gmail.com>
2021-08-29 19:53:35 +0900

    Fix redis image

--- config/config.py
+++ config/config.py
@@ -6,7 +6,7 @@ env.read_env()
 TOKEN = env('TG_TOKEN')
 URL = env('URL')
 ADMIN_TG_ID = env('ADMIN_TG_ID')
-REDIS_URL = env('REDIS_URL')
+REDIS_URL = env('REDIS_URL', 'redis://localhost:6379')
 
 URL_BOOKS_LIB = env('URL_BOOKS_LIB')
 
