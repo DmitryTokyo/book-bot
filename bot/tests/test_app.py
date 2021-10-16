60e0441eac7d87ab332491f9ebd09ed3d9d404a3
andimeon-ubuntu <andimeon@gmail.com>
2021-10-16 22:06:22 +0900

    Add test_check_template, test_webhook_set_template

--- bot/tests/test_app.py
+++ bot/tests/test_app.py
@@ -1,4 +1,28 @@
+import pytest
 
-def test_check(client):
+
+def test_check_template(client):
+    template_data = b'Well done!'
+
     response = client.get('/')
+
     assert response.status_code == 200
+    assert template_data in response.data
+
+
+@pytest.mark.parametrize(
+    'template_data, webhook_result',
+    [
+        (b'webhook setup ok!!!', 'webhook'),
+        (b'webhook setup failed', None),
+    ],
+)
+def test_webhook_set_template(mocker, client, template_data, webhook_result):
+    mock_result = mocker.patch('bot.app.bot')
+    mock_result.setWebhook.return_value = webhook_result
+    mocker.patch('bot.app.os')
+
+    response = client.get('/webhook')
+
+    assert response.status_code == 200
+    assert template_data in response.data
